bbf78903e8c1d03182b95b1cc47735380e34711e
oyiadin <oyiadin@gmail.com>
2018-09-02 15:11:34 +0800

    调了个比较好的观赏体验 XD

--- example/sgd_vs_momentum.py
+++ example/sgd_vs_momentum.py
@@ -4,34 +4,35 @@ import matplotlib.pyplot as plt
 import bitflow as bf
 
 
+f = lambda x, y: x ** 2 + 5 * y ** 2
+
+
 plt.ion()
-plt_x = np.linspace(0, 1.5, 20)
-plt_y = np.linspace(0, 2, 20)
+plt_x = np.linspace(-0.5, 1.2, 40)
+plt_y = np.linspace(-0.5, 1.2, 40)
 plt_x, plt_y = np.meshgrid(plt_x, plt_y)
-
-height = lambda x, y: (x - 2) ** 2 + 2 * (y - 2) ** 2 + x * y
-
+plt_z = f(plt_x, plt_y)
+plt.contourf(plt_x, plt_y, plt_z, 25)
 
 def paint(p1, p2):
-    plt.contourf(plt_x, plt_y, height(plt_x, plt_y), 6)
-    plt.scatter(*p1, color='red', marker='o')
-    plt.scatter(*p2, color='green', marker='x')
+    plt.scatter(*p1, color='red')
+    plt.scatter(*p2, color='green')
     plt.pause(0.015)
 
-paint((0.5, 0.5), (0.5, 0.5))
-time.sleep(1)
+paint((1, 1), (1, 1))
+time.sleep(1.5)
 
 
 with bf.Session() as sess:
-    x1 = bf.Variable(0.5)
-    y1 = bf.Variable(0.5)
-    z1 = (x1 - 2) ** 2 + 2 * (y1 - 2) ** 2 + x1 * y1
+    x1 = bf.Variable(1)
+    y1 = bf.Variable(1)
+    z1 = f(x1, y1)
     op1 = bf.train.GradientDescentOptimizer(0.0035).minimize(z1)
 
-    x2 = bf.Variable(0.5)
-    y2 = bf.Variable(0.5)
-    z2 = (x2 - 2) ** 2 + 2 * (y2 - 2) ** 2 + x2 * y2
-    op2 = bf.train.MomentumOptimizer(0.0035, 0.85).minimize(z2)
+    x2 = bf.Variable(1)
+    y2 = bf.Variable(1)
+    z2 = f(x2, y2)
+    op2 = bf.train.MomentumOptimizer(0.0035, 0.8).minimize(z2)
 
     for i in range(200):
         paint(sess.run(x1, y1), sess.run(x2, y2))
